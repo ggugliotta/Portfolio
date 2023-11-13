7719dc7a10da09bd6127fdeb3498c38dcc37843e
Gabriella Gugliotta <gabriellagugliotta96@gmail.com>
2023-11-13 12:03:00 -0600

    added calendar

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -16,7 +16,9 @@ const Footer = ({}) => {
             <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
               TOGETHER
             </h1>
+            <a href="https://calendly.com/gabriellagugliotta" target="_blank" rel="noopener noreferrer">
               <Button type="primary">Schedule a call</Button>
+            </a>
             <div className="mt-10">
               <Socials />
             </div>
