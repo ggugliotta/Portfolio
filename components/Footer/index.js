4388ddbe192aff037d4ac0727fc5b40b2f09d364
Gabriella Gugliotta <gabriellagugliotta96@gmail.com>
2023-11-13 11:47:39 -0600

    changed button

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -16,9 +16,7 @@ const Footer = ({}) => {
             <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
               TOGETHER
             </h1>
-            <Link to ="https://calendly.com/gabriellagugliotta">
               <Button type="primary">Schedule a call</Button>
-            </Link>
             <div className="mt-10">
               <Socials />
             </div>
